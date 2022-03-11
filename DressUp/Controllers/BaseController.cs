cb25e9c0853ef84f08c013dd087943e515b18617
Darka23 <darka.2004@abv.bg>
2022-03-11 22:39:10 +0200

    Login Register and Logout pages working

---
+++ DressUp/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DressUp.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+
+    }
+}
