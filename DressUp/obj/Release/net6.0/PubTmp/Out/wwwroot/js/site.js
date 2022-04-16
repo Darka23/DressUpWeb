7e92579dc09a14656d61a45a63161334e2b5f30a
Darka23 <darka.2004@abv.bg>
2022-04-16 17:08:45 +0300

    Hosted app in Azure

---
+++ DressUp/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿
+
+function AddToCart(type, id) {
+
+    const obj = {
+        objId: type+id,
+        type: type,
+        id: id
+    };
+
+    let localStorageArray = [];
+
+    if (localStorage.getItem("Cart")) {
+        localStorageArray = JSON.parse(localStorage.getItem("Cart"))
+    }
+
+    if (localStorageArray.filter(x => x.objId == obj.objId).length <= 0) {
+        localStorageArray.push(obj);
+    }
+   
+    localStorage.setItem("Cart", JSON.stringify(localStorageArray));
+
+}
